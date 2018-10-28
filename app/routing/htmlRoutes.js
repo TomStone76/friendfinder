cc43a1598fb9f4d68e20b78af5e6c4c27afcb587
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-28 17:23:09 -0400

    commit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/tables.html"));
     });
 
-    app.get("*", function(req res) {
+    app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"))
     });
 
