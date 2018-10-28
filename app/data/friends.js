b59795603d997f4570fdce2997b611075b2227a4
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-28 14:30:20 -0400

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsData = [
+    {
+        name: "John Doe",
+        photo: "https://en.wikipedia.org/wiki/Smiley#/media/File:Breathe-face-smile.svg",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    }
+]
+
+module.exports = friendsData;
