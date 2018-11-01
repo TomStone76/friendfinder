c8ae0fc5e17248e324b0ae0b0f52dc4414f4ab35
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-01 14:30:39 -0400

    commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,43 @@
-var friends = require("../data/friends");
+var friendsData = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friendsData);
     });
 
-    app.post("/api/friends", function(req, res) {
+
+    function detectNum(obj) {
+        for (var index in obj) {
+            if (!isNaN(obj[index])) {
+                obj[index] = Number(obj[index]);
+            } else if (typeof obj === "object") {
+                detectNum(obj[index]);
+            }
+        }
+    }
+
+    // app.post("/api/friends", function (req, res) {
+    //     console.log("post route working");
+    //     console.log(req.body);
+    //     friendsData.push(req.body);
+    //     console.log(friendsData)
+    //     for (var i = 0; i < friendsData.length; i++) {
+    //         var arrNums = friendsData[i].scores.map(Number);
+    //         console.log(arrNums);
+    //     }
+    //     console.log(friendsData.slice(-1)[0]);
+    //     res.json(friendsData[0]);
+    // });
+
+
+    app.post("/api/friends", function (req, res) {
         console.log("post route working");
-        console.log(req.body);
         friendsData.push(req.body);
+        for (var i = 0; i < friendsData.length; i++) {
+            var arrNums = friendsData[i].scores.map(Number);
+            console.log(friendsData[i].name);
+            console.log(arrNums);
+        }
     });
 }
